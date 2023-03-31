ffc5f26248161116fbd50a2ce05c0cf969fdd81c
sara0408 <sara.poljicak@gmail.com>
2023-03-31 18:16:30 +0200

    created join lobby page

--- Client/pages/lobby.jsx
+++ Client/pages/lobby.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/JoinLobby.module.css';
 import { Roboto } from '@next/font/google';
 import { useState } from 'react';
-
+import {Image} from "next"
 const roboto = Roboto({ 
     weight: '400',
     subsets: ['latin'],
@@ -18,7 +18,30 @@ return (
         `}</style>
 
         <main className={styles.mainContainer}>
+        <nav className={styles.navbar}>
+            <button className={styles.logoutButton}>Log out</button>
+        </nav>
 
+        <div className={styles.centerContainer}>
+            <div className={styles.statisticsContainer}>
+                <div className={styles.totalWinsContainer}>
+                    <img className={styles.totalWinsImg} src="/win.png"></img>
+                    <p>Total wins: 75</p>
+                </div>
+                <div className={styles.winRateContainer}>
+                    <img className={styles.winRateImg} src="/win-rate.png"></img> 
+                    <p>Correct Rate: 50.00%</p>
+                </div>
+                <div className={styles.correctRateContainer}>
+                    <img className={styles.correctRateImg} src="/correct-rate.png"></img>
+                    <p>Correct Rate: 70.00%</p>
+                </div>
+            </div>
+            <div className={styles.buttonContainer}>
+                <button className={styles.joinLobbyButton}>Join Lobby</button>
+                <button className={styles.createLobbyButton}>Create Lobby </button>
+            </div>
+        </div>
         </main>
 
     </>
